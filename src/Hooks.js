2107bf42197cf5c82f1981c5761b14a4c8503800
Hamza Sheikh <hrsheikh02@gmail.com>
2022-06-09 18:00:27 +0400

    created a seperate handleClick function and added to the button

--- src/Hooks.js
+++ src/Hooks.js
@@ -2,8 +2,9 @@ import React, { Component, useState } from 'react';
 
 const HookCreation = () => {
     const [counter, setCounter] = useState(5);
+    const handleClick = () => setCounter(counter * 2);
     return(
-        <button onClick={()=> setCounter(counter * 2)}>
+        <button onClick={handleClick}>
             {counter}
         </button>
     )
